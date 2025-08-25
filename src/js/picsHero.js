feac57c3f9e540c7f24d7d9e4677d0e5609a3880
cerrpuffs <crgraft503@gmail.com>
2025-08-24 18:06:28 -0600

    primer vistazo

---
+++ src/js/picsHero.js
@@ -0,0 +1,10 @@
+const picHero = [
+  {
+    link: "https://i.pinimg.com/736x/db/17/76/db17767d406973b406f5ec87b543403f.jpg",
+    alt: "persona con ropa de moda"
+  },
+  {
+    link: "https://i.pinimg.com/736x/61/8d/9f/618d9f45b53d1ecc08dae763541f9ded.jpg",
+    alt: "persona con ropa de moda"
+  }
+];
